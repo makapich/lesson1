3b7705ba7879d7476e162693831473991aa6d435
makapich <makarovtima05@gmail.com>
2023-02-16 19:41:31 +0100

    Link change

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-
+    assert parse('https://example.com/path/to/page?name=+380502924354&color=2+2') == {'name': '380502924354', 'color': '2 2'}
+    assert parse('https://example.com/path/to/page?name=&&color=&&exempe==name=') == {'name': '', 'color': '', 'exemple': '=name='}
+    assert parse('https://example.com/path/to/page?name=&&color=&&exempe=?name?') == {'name': '', 'color': '', 'exemple': '?name?'}
+    assert parse('https://example.com/path/to/page?name=?/&color=/&&exempe=?name=/') == {'name': '?/', 'color': '/', 'exemple': '?name=/'}
+    assert parse('https://example.com/path/to/page?name=?/&color=/p&exempe=?name=/&&main=1000') == {'name': '?/', 'color': '/p', 'exemple': '?name=/', 'main': '1000'}
+    assert parse('https://example.com/path/to/page?name=https://example.com/') == {'name': 'https://example.com/'}
+    assert parse('https://example.com/path/to/page?name=діма&color=червоний') == {'name': 'діма', 'color': 'червоний'}
+    assert parse('https://example.com/path/to/page?name=/&color=/') == {'name': '/', 'color': '/'}
+    assert parse('https://example.com/path/to/page?name:/&/=dima') == {'name': '', '/': 'dima'}
+    assert parse('https://example.com/path/to/page?name:/?/= ?dima?') == {'name:/?/': '?dima?'}
 
 def parse_cookie(query: str) -> dict:
     return {}
